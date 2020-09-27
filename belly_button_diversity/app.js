89acff5a6eaf41f12f58d348a5c1e080a54dc2a5
ryann-green <65695678+ryann-green@users.noreply.github.com>
2020-09-26 23:26:27 -0500

    updated default page

--- belly_button_diversity/app.js
+++ belly_button_diversity/app.js
@@ -20,6 +20,86 @@ d3.json("data/data.json").then((importedData) => {
 
     });
 
+     // select the value of the property in the dropdown
+     let dName = dropdownmenu.property("value");
+
+     // create a filter function to allow the id of the sample to equal the value in the dropdown box
+     function filterSamples(i) {
+
+         // return the id that is equal to the dropdown value option
+         return i.id == dName
+     };
+ 
+     // run the filter on the dataset
+     let filteredSamples = data.samples.filter(filterSamples)[0]
+
+     let sampleValues = filteredSamples.sample_values;
+     let otuIDs = filteredSamples.otu_ids;
+     let otuLabels = filteredSamples.otu_labels;
+
+     // bar & bubble chart variables
+
+     // this wil be the values for the bar chart
+     // this wil also be the y valyes and marker size for the bubble chart
+     let topTenValues = sampleValues.slice(0,10);
+     
+     // this will be the labels for the bar chart
+     // this will also be the x values and the marker colors for the bubble chart
+     let topTenOtuIDs = otuIDs.slice(0,10);
+     let OtuIDstrings = topTenOtuIDs.map(elem => `otu: ${elem}`);
+
+     // this will be the hovertext for the bar chart
+     // this will also be the text values for the bubble chart
+     let topTenLabels = otuLabels.slice(0,10);
+     
+     let trace1 = {
+         x: topTenValues.reverse(),
+         y: OtuIDstrings.reverse(),
+         type: "bar",
+         orientation: "h"
+     };
+
+     let stuff = [trace1];
+
+     let layout = {
+         title: "Otu Sample values",
+         xaxis: { title: "Sample Values" },
+         yaxis: { title: "OTU IDs"}
+     };
+
+     Plotly.newPlot("barh", stuff, layout);
+
+     var trace2 = {
+         x: otuIDs,
+         y: sampleValues,
+         mode: 'markers',
+         marker: {
+           size: sampleValues
+         }
+       };
+       
+       let bubble1 = [trace2];
+       
+       var layout2 = {
+         title: 'Sample Values vs OTU IDs',
+         showlegend: false,
+         xaxis: { title: "OTU IDs" },
+         yaxis: { title: "Sample Values"}
+      
+       };
+       
+       Plotly.newPlot('bubble', bubble1, layout2);
+
+     let metadata = data.metadata.filter(filterSamples)[0]
+
+     let metadatahtml = d3.select("#sample-metadata");
+
+     
+     console.log(metadata);
+
+
+
+
     // Call updatePlotly() when a change takes place to the DOM
     d3.select("#selDataset").on("change", updatePlotly); 
 
@@ -43,10 +123,6 @@ d3.json("data/data.json").then((importedData) => {
         let otuIDs = filteredSamples.otu_ids;
         let otuLabels = filteredSamples.otu_labels;
 
-        // console.log(sampleValues)
-        // console.log(otuIDs )
-        // console.log(otuLabels)
-
         // bar & bubble chart variables
 
         // this wil be the values for the bar chart
@@ -61,15 +137,6 @@ d3.json("data/data.json").then((importedData) => {
         // this will be the hovertext for the bar chart
         // this will also be the text values for the bubble chart
         let topTenLabels = otuLabels.slice(0,10);
-
-      
-        // console.log(topTenValues);
-        // console.log(OtuIDstrings);
-        // console.log(topTenLabels);
-
-        // console.log(`Id is ${dName}, Top 10 sample values are ${topTenValues}`);
-        // console.log(`Id is ${dName}, Top 10 sample values are ${topTenOtuIDs}`);
-        // console.log(`Id is ${dName}, Top 10 sample values are ${topTenLabels}`);
         
         let trace1 = {
             x: topTenValues.reverse(),
@@ -122,18 +189,3 @@ d3.json("data/data.json").then((importedData) => {
     };
 
 });
-
-
-
-
-
-
-
-
-// function updatePlotly() {
-//     // Use D3 to select the dropdown menu
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var dataset = dropdownMenu.property("value");
-
-// need slice, filter, and map functions
